5eec5fe1933bb226297c1e617d8faa99b1038c31
lixiaohao <lixiaohaono1@163.com>
2017-03-13 10:18:19 +0800

    修改ReadUtils

---
+++ src/main/java/com/lixiaohao/poi/innerInner.java
@@ -0,0 +1,24 @@
+package com.lixiaohao.poi;
+
+/**
+ * Created by lixiaohao on 2017/3/10
+ *
+ * @Description
+ * @Create 2017-03-10 14:18
+ * @Company
+ */
+public class innerInner {
+    public innerInner() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+}
